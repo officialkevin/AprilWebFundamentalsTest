1861179354909cc1037955c9ee0495716934bf43
officialkevin <102136603+officialkevin@users.noreply.github.com>
2022-05-29 21:01:03 +0530

    Add files via upload

---
+++ test6/question-3.js
@@ -0,0 +1,9 @@
+var length=4;
+var breadth= 4;
+
+if(length===breadth){
+  console.log('square');
+}
+else{
+  console.log('rectangle');
+}
---
+++ test6/question-5.js
@@ -0,0 +1,27 @@
+//initializing a variable
+let day=2;
+
+
+switch(day){
+  case 1:
+  console.log('sunday');
+  break;
+   case 2:
+  console.log('monday');
+  break;
+   case 3:
+  console.log('tuesday');
+  break;
+   case 4:
+  console.log('wednesday');
+  break;
+   case 5:
+  console.log('thursday');
+  break;
+   case 6:
+  console.log('friday');
+  break;
+   case 7:
+  console.log('saturday');
+  break;
+}
